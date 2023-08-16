db249cc01fd1e852770e75642f09b026ddf5f3e2
XArcangelll <Xx-Arcangel@hotmail.com>
2023-08-15 19:40:43 -0500

    ayayayay

--- global/config.php
+++ global/config.php
@@ -13,5 +13,5 @@ define("DESCARGASPERMITIDAS","1");
 
 
 define("LINKAPI","https://api-m.sandbox.paypal.com");
-define("CLIENTID","AVo3YO-4mEEOE8wQ66xuhbeZ-EPaq3nMZdNAzt3LbZ6jDjm7gBIy1-7a4zq9xRfUa_EAKMqZCSTqXmF6");
-define("SECRET","EBGnBjA2AMDTNkDKaClHS9VDxiU9Vd1GvdRlTyVZsxGLm74gOnKUhKJEghcExFYDKKy0MUekjrGHf3bj");
+define("CLIENTID",""); // ID DE CLIENTE EN PAYPAL
+define("SECRET",""); //SECRET ID DE PAYPAL
